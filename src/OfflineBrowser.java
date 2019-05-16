83e3936c5fd2c9ab98dcad4c99343b44e9418268
THẠCH NGUYỄN QUANG <1612602@student.hcmus.edu.vn>
2019-05-16 15:02:44 +0700

    Minor fixes

--- src/OfflineBrowser.java
+++ src/OfflineBrowser.java
@@ -5,6 +5,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class OfflineBrowser {
     private String url;
@@ -13,19 +15,41 @@ public class OfflineBrowser {
         url = param;
     }
 
+    private boolean isConnectionAvailable() {
+        try {
+            //Create new URL
+            URL urlObj = new URL(url);
+            //Create new connection
+            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+            //Set request method
+            con.setRequestMethod("GET");
+            //Set connection timeout
+            con.setConnectTimeout(5000);
+            //Connect
+            con.connect();
+            //Compare response code, connection accepted if code is 200
+            //Reference here: https://docs.oracle.com/javase/7/docs/api/java/net/HttpURLConnection.html
+            if (con.getResponseCode() == 200) {
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
     public void Start() {
-        if (url.equals("")) {
-            JOptionPane.showMessageDialog(null, "Empty input!");
-        } else if (!isConnectionAvailable()) {
+        if (!isConnectionAvailable()) {
             JOptionPane.showMessageDialog(null, "Connection failed, URL may not exist or network is not available!");
         } else {
             try {
                 URL website = new URL(url);
                 ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-                String fileName = "homepage.html";
+                String fileName = "output\\homepage.html";
                 FileOutputStream fos = new FileOutputStream(fileName);
                 fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
                 fos.close();
+                getAllLinks(url);
                 JOptionPane.showMessageDialog(null, "This works out! Site saved to " + fileName);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -33,22 +57,25 @@ public class OfflineBrowser {
         }
     }
 
-    private boolean isConnectionAvailable() {
-        try {
-            URL urlObj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
-            con.setRequestMethod("GET");
-            // Set connection timeout
-            con.setConnectTimeout(3000);
-            con.connect();
-            int responseCode = con.getResponseCode();
-            if (responseCode == 200) {
-                return true;
+    private void getAllLinks(String urlParam) throws IOException {
+        ArrayList<String> al = new ArrayList<String>();
+        URL pageLocation = new URL(urlParam);
+        Scanner in = new Scanner(pageLocation.openStream());
+        while (in.hasNext()) {
+            String line = in.next();
+            // "href=\"https://"
+            if (line.contains("href=\"")) {
+                int from = line.indexOf("\"");
+                int to = line.lastIndexOf("\"");
+                al.add(line.substring(from + 1, to));
             }
-        } catch (Exception e) {
-            return false;
         }
-        return true;
+        FileOutputStream fs = new FileOutputStream("output\\output.txt");
+        for (String s : al) {
+            fs.write(s.getBytes());
+            fs.write('\n');
+        }
+        fs.close();
     }
 
 }
