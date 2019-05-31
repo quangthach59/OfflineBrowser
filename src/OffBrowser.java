8e241bcfc4127d5d72b40038edc948a5016e8a7f
THẠCH NGUYỄN QUANG <1612602@student.hcmus.edu.vn>
2019-05-31 10:13:05 +0700

    Optimize code

--- src/OffBrowser.java
+++ src/OffBrowser.java
@@ -1,14 +1,11 @@
-import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
-import com.sun.org.apache.xpath.internal.functions.FuncStringLength;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
 import javax.swing.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,12 +13,22 @@ public class OffBrowser {
     //Format https://www.example.com/ or https://example.com/
     private String url;
     private String homepageWithSlash;
+    private ArrayList<String> listURLs;
+    private final String DEFAULT_FOLDER_OUTPUT = "output";
+    private final String DEFAULT_HOMEPAGE_FILENAME = "default.html";
 
     public OffBrowser(String param) {
         url = param;
         homepageWithSlash = removeProtocol(url);
+        listURLs = new ArrayList<>();
     }
 
+    /**
+     * Remove prefix http(s):// and www.
+     *
+     * @param param input URL
+     * @return URL without prefix
+     */
     private String removeProtocol(String param) {
         String result = param;
         for (String s : new String[]{"http://", "https://", "www."}) {
@@ -32,44 +39,65 @@ public class OffBrowser {
         return result;
     }
 
+    /**
+     * Get a string containing children of a standard URL
+     *
+     * @param param input URL
+     * @return a string containing children of a standard URL
+     */
     private String getStringChildren(String param) {
         String result = param;
         result = removeProtocol(result);
+        int indexOfSlash = result.indexOf("/");
+        //For example, https://www.hcmus.edu.vn lacks the last slash
+        if (indexOfSlash == -1)
+            return "";
         return result.substring(result.indexOf("/") + 1);
     }
 
+    /**
+     * Get file name from a given URL
+     *
+     * @param param input URL
+     * @return file name, also creates directories to save this file
+     */
     private String getFullFileName(String param) {
         //Filename : \\
         //Directory: /
         String result = getStringChildren(param);
-        String fullFileName = "";
-        String folderName = "output/";
+        StringBuilder fullFileName = new StringBuilder();
+        StringBuilder folderName = new StringBuilder(DEFAULT_FOLDER_OUTPUT + "/");
         boolean isAFile = result.contains(".");
-        //Nothing after homepage
+        //Nothing after homepage && not a file && the same as homepage with slash
         if (!isAFile && result.equals("") && removeProtocol(param).equals(homepageWithSlash)) {
-            boolean bool = new File(folderName).mkdirs();
-            return "default.html";
+            boolean bool = new File(folderName.toString()).mkdirs();
+            return DEFAULT_HOMEPAGE_FILENAME;
         }
         while (result.contains("/")) {
             String part = result.substring(0, result.indexOf("/"));
-            fullFileName += part + "\\";
-            folderName += part + "/";
+            fullFileName.append(part).append("\\");
+            folderName.append(part).append("/");
             result = result.substring(part.length() + 1);
         }
         if (result.equals("")) {
-            fullFileName = fullFileName.substring(0, fullFileName.length() - 1);
-            fullFileName += ".html";
+            fullFileName = new StringBuilder(fullFileName.substring(0, fullFileName.length() - 1));
+            fullFileName.append(".html");
         } else if (result.contains(".")) {
-            fullFileName += result;
+            fullFileName.append(result);
         } else {
-            fullFileName += result + ".html";
+            fullFileName.append(result).append(".html");
         }
-        File folder = new File(folderName);
+        File folder = new File(folderName.toString());
         boolean bool = folder.mkdirs();
-
-        return fullFileName;
+        return fullFileName.toString();
     }
 
+    /**
+     * Determine if an URL is available for connection
+     *
+     * @param param input URL
+     * @return true if available, false if not
+     */
     private boolean isConnectionAvailable(String param) {
         try {
             URL urlObj = new URL(param);
@@ -79,100 +107,116 @@ public class OffBrowser {
             con.connect();
             //Compare response code, connection accepted if code is 200
             //Reference here: https://docs.oracle.com/javase/7/docs/api/java/net/HttpURLConnection.html
-            if (con.getResponseCode() == 200) {
-                return true;
-            }
+            return (con.getResponseCode() == 200);
         } catch (Exception e) {
             return false;
         }
-        return true;
     }
 
-    private void DownloadHomePage(String param) throws IOException {
-        URL page = new URL(param);
-        Scanner in = new Scanner(page.openStream());
-        String fileName = getFullFileName(param);
-        String currentPage = removeProtocol(param);
-        String fileDefault = getFullFileName(url);
-        if (!currentPage.endsWith("/")) {
-            currentPage += "/";
-        }
-        FileOutputStream fos = new FileOutputStream("output\\" + fileName);
-        while (in.hasNext()) {
-            String line = in.nextLine();
-//            //Replace current URL with current file path
-            line = line.replaceAll("https://www." + currentPage + "\"", fileName + "\"");
-            line = line.replaceAll("https://" + currentPage + "\"", fileName + "\"");
-            line = line.replaceAll("http://" + currentPage + "\"", fileName + "\"");
-
-            //Remove homepage with default file
-            line = line.replaceAll("https://www." + homepageWithSlash + "\"", fileDefault + "\"");
-            line = line.replaceAll("https://" + homepageWithSlash + "\"", fileDefault + "\"");
-            line = line.replaceAll("http://" + homepageWithSlash + "\"", fileDefault + "\"");
-
-            //
-            line = line.replaceAll("https://www." + homepageWithSlash, "");
-            line = line.replaceAll("https://" + homepageWithSlash, "");
-            line = line.replaceAll("http://" + homepageWithSlash, "");
-//            ArrayList<String> list = new ArrayList<String>();
-//            GetAllLinks(param,list);
-//            ReplaceInLocalFile(line, list);
-            fos.write(line.getBytes());
-        }
-        fos.close();
-        System.out.println("output\\" + fileName);
-    }
-
-    private void ReplaceInLocalFile(String line, ArrayList<String> list) {
-        for (String s : list) {
-            String currentPage = removeProtocol(s);
-            String fileName = getFullFileName(s);
-            //Replace current URL with current file path
-            line = line.replaceAll("https://www." + currentPage + "\"", fileName + "\"");
-            line = line.replaceAll("https://" + currentPage + "\"", fileName + "\"");
-            line = line.replaceAll("http://" + currentPage + "\"", fileName + "\"");
-
-            line = line.replaceAll("https://www." + homepageWithSlash, "");
-            line = line.replaceAll("https://" + homepageWithSlash, "");
-            line = line.replaceAll("http://" + homepageWithSlash, "");
-
-        }
-    }
-
-    private void GetAllLinks(String param, ArrayList<String> list) throws IOException {
-        URL pageLocation = new URL(param);
-        Scanner in = new Scanner(pageLocation.openStream());
+    private void DownloadPage(String param) throws Exception {
+        Scanner in = new Scanner(new URL(param).openStream());
+        String fileOutput = getFullFileName(param);
+        //Content will be saved to this file
+        FileOutputStream fos = new FileOutputStream(DEFAULT_FOLDER_OUTPUT + "\\" + fileOutput);
+        ArrayList<String> list = new ArrayList<>();
+        //Scan each line for href
         while (in.hasNext()) {
             String line = in.next();
+            //Start of an URL
             if (line.contains("href=\"")) {
-                try {
-                    int from = line.indexOf("\"");
-                    int to = line.lastIndexOf("\"");
-                    String result = line.substring(from + 1, to);
-                    if (removeProtocol(result).indexOf(homepageWithSlash) == 0 && !list.contains(result)) {
-
-                        list.add(result);
-                    }
-                } catch (Exception e) {
-                    System.out.print("Error occurred with URL(s). Offline site may not work properly.");
+                //Split from href=" to the closest "
+                line = line.substring(line.indexOf("\"") + 1, line.lastIndexOf("\""));
+                //If this URL is necessary and not duplicated, add to list
+                if (LinkNeedsAdding(line) && !list.contains(line)) {
+                    list.add(line);
+                    saveFile(line);
                 }
             }
         }
+        //Close scanner and renew
+        //Read the whole content for find and replace, then save
+        in.close();
+        in = new Scanner(new URL(param).openStream());
+        while (in.hasNext()) {
+            //The whole content of the page as plain text
+            String line = in.nextLine();
+            line = ReplaceURLWithURL(line, list);
+            fos.write(line.getBytes());
+        }
+        fos.close();
     }
 
-    public void StartBrowsing() {
+    private void saveFile(String param) throws IOException {
+        try {
+            if (param.startsWith("/"))
+                param = url + param.substring(1);
+            URL url = new URL(param);
+            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+            String destName = getFullFileName(param);
+            FileOutputStream fos = new FileOutputStream(DEFAULT_FOLDER_OUTPUT + "\\" + destName);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            fos.close();
+            System.out.println("Saved to " + DEFAULT_FOLDER_OUTPUT + "\\" + destName);
+        } catch (IOException e) {
+            System.out.println("Error saving file " + param);
+        }
+
+    }
+
+    private String ReplaceURLWithURL(String line, ArrayList<String> list) {
+        line = line.replaceAll("href=\"" + url, "href=\"" + getFullFileName(url));
+        for (String s : list) {
+            //System.out.println("Replacing " + s);
+            line = line.replaceAll("href=\"" + s, "href=\"" + getFullFileName(s));
+        }
+        return line;
+    }
+
+    /**
+     * Determine if a link should be added for browsing
+     *
+     * @param param input URL
+     * @return true if needed, false if not
+     */
+    private boolean LinkNeedsAdding(String param) {
+        //URL is empty or URL starts with # (fragment), don't add
+        if (param.equals("") || param.equals("/") || param.contains("#") || param.contains("?"))
+            return false;
+        //URL is sub link, add
+        if (param.startsWith("/"))
+            return true;
+        //Try to download and replace .css file may leads to crashing UI
+        if (param.endsWith(".css"))
+            return false;
+        //Sub-string after the last slash contains a dot, a present of file extension, add
+        //if (param.substring(param.lastIndexOf("/")).contains("."))
+        if (getStringChildren(param).contains("."))
+            return true;
+        try {
+            URL urlHome = new URL(url);
+            URL urlParam = new URL(param);
+            //URL and homepage share the same host, but not equal, add
+            if (urlHome.getHost().equals(urlParam.getHost()) && !removeProtocol(url).equals(removeProtocol(param)))
+                return true;
+        } catch (Exception e) {
+            return false;
+        }
+        //Return for fun, or any unknown kind of URL
+        return false;
+    }
+
+    /**
+     * Start browsing
+     */
+    void StartBrowsing() {
         if (!isConnectionAvailable(url)) {
             JOptionPane.showMessageDialog(null, "Error parsing input to URL");
         } else {
             try {
-                DownloadHomePage(url);
-                ArrayList<String> list = new ArrayList<String>();
-                GetAllLinks(url, list);
-                for (String s : list) {
-                    DownloadHomePage(s);
-                    System.out.println(s);
-                }
-                JOptionPane.showMessageDialog(null, "Done!");
+                long startTime = System.currentTimeMillis();
+                DownloadPage(url);
+                long endTime = System.currentTimeMillis();
+                JOptionPane.showMessageDialog(null, "Done! Time lapsed: " + (endTime - startTime) + "ms");
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.print("Error with StartBrowsing");
